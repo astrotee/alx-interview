809d6515674475573feb8259e4ececc878d8e758
amin ahmed <aminahmed24@gmail.com>
2024-10-06 14:41:27 +0300

    0. Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""""pascals triangle"""
+
+
+def pascal_triangle(n):
+    "generate pascal triangle of hight n"
+
+    if n <= 0:
+        return []
+    elif n == 1:
+        return[[1]]
+    elif n == 2:
+        return [[1],[1,1]]
+
+    tri = pascal_triangle(n-1)
+    new_row = [1] * n
+    last_row = tri[-1]
+    for i in range(1, n-1):
+        new_row[i] = last_row[i-1] + last_row[i]
+    tri.append(new_row)
+    return tri
