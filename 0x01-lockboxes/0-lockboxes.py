2b0f27d6b1cfa5f923196fc6931e66dd2ce4d26d
amin ahmed <aminahmed24@gmail.com>
2024-10-10 14:41:01 +0300

    0. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""""lockboxes"""
+
+
+def canUnlockAll(boxes: list):
+    "check if all boxes are reachable"
+    unvisited = set(range(1, len(boxes)))
+    dfs(boxes[0], boxes, unvisited)
+    return len(unvisited) == 0
+
+
+def dfs(box: list, boxes: list, unvisited: set):
+    "do a dfs on boxes"
+    for key in box:
+        if key in unvisited and key in range(1, len(boxes)):
+            unvisited.remove(key)
+            dfs(boxes[key], boxes, unvisited)
